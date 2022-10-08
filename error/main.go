8d55f477e6f0ff53bbde92cb63fcc205a77fe490
陈思琳 <chensilin@chensilindeMacBook-Air.local>
2022-10-09 01:41:41 +0800

    fix:错误处理

--- error/main.go
+++ error/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
+
+	errors1 "github.com/pkg/errors"
 )
 
 func main() {
-	NewError()
+	// NewError()
+	TestErrors()
 }
 
 // err的创建
@@ -29,4 +32,42 @@ func NewError() {
 	e4 := errors.New("错误")
 	fmt.Println(e1 == e2)
 	fmt.Println(e3 == e4)
+}
+
+// errors第三方库的使用
+func TestErrors() {
+	num := 1
+	// 输出错误信息，不包含堆栈
+	fmt.Printf("%s\n", a(num))
+	fmt.Printf("%T %v\n", errors1.Cause(a(num)), errors1.Cause(a(num)))
+	// 输出带引号的错误信息，不包含堆栈
+	fmt.Printf("%q\n", a(num))
+	// 输出错误信息和堆栈
+	fmt.Printf("%T %+v\n", a(num), a(num))
+}
+func a(num int) error {
+	switch num {
+	case 1:
+		return b()
+	case 2:
+		return c()
+	case 3:
+		return d()
+	}
+	return nil
+}
+
+func b() error {
+	// return errors1.New("new error")
+	aa := "hh"
+	return errors1.Errorf("new error %s", aa)
+}
+
+func c() error {
+	err := errors.New("aaa")
+	return errors1.Wrap(err, "warp error")
+}
+func d() error {
+	err := errors.New("aaa")
+	return err
 }
