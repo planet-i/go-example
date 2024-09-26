0130bab34b6bcc8effcbf58349731ff232dd29e6
planet-i <1979874140@qq.com>
2024-09-26 17:56:44 +0800

    插入图片

--- excelize/main.go
+++ excelize/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	_ "image/gif"
+	_ "image/jpeg"
 	_ "image/png"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
+	excelizev2 "github.com/xuri/excelize/v2"
 )
 
 func main() {
+	TestImage()
+
 	f := excelize.NewFile()             //新建excel文件
 	f.SetCellValue("Sheet1", "B2", 100) //填值
 	f.SetCellValue("Sheet1", "A1", 90)
@@ -61,3 +72,110 @@ func main() {
 /* AddChart("当前工作表名", "图表左上角在表中的位置", `{"type":"图标类型",
 "series":[{"name":"某类","categories":"x方向上的值的范围","values":"某类在x上对应的y值"},{"name":"Sheet1!$A$3","categories":"Sheet1!$B$1:$D$1","values":"Sheet1!$B$3:$D$3"},{"name":"Sheet1!$A$4","categories":"Sheet1!$B$1:$D$1","values":"Sheet1!$B$4:$D$4"}],
 "title":{"name":"图表名称"}}`) */
+
+func testImage() {
+	// 创建一个新的Excel文件，并指定活跃的工作表
+	activeSheet := "Sheet1"
+	f := excelizev2.NewFile()
+	index, _ := f.NewSheet(activeSheet)
+	// 写表头
+	colNos := make([]string, 0)
+	colNos = append(colNos, []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V"}...)
+	headers := []string{"SPU", "图片"}
+	for k, v := range headers {
+		col := fmt.Sprintf("%v1", colNos[k])
+		f.SetCellValue(activeSheet, col, v)
+	}
+	// 数据源
+	res := [][]string{
+		{"data1", "https://xiaowandou-ecom.s3.ap-southeast-1.amazonaws.com/upload/8b21d4e83432fe5d96638945baed3ee7.jpg"},
+		{"data2", "https://cdn.unitemagic.com/upload/337c13d813fe46361f9b73f3558678a8.jpeg"},
+		{"data3", "https://imgcdn.umcasual.com/um_ecom/66805433/upload/800d8a8ffc66878297772f389c733bf7_e772d785-0021-4521-bd55-1eae7c815adb.png"},
+	}
+	imageCol := GetImageCol(headers)
+	// 插入数据和图片
+	for i, row := range res {
+		f.SetRowHeight(activeSheet, i+2, 60)
+		for j, data := range row {
+			col := fmt.Sprintf("%s%d", colNos[j], i+2) // 简化列号生成逻辑
+			fmt.Println(col, data)
+			if colNos[j] == imageCol && data != "" {
+				locked := true
+				format := &excelizev2.GraphicOptions{
+					LockAspectRatio: true,
+					Locked:          &locked,
+					Positioning:     "oneCell",
+					AutoFit:         true,
+				}
+
+				// 读取远程图片
+				image, err := ReadRemoteFile(data)
+				if err != nil {
+					log.Printf("ReadRemoteFile err: %s", err)
+				}
+				ext := GetImageExt(data)
+				fmt.Println(ext)
+				if err := f.AddPictureFromBytes(activeSheet, col, "image", image, format); err != nil {
+					log.Printf("f.AddPictureFromBytes err: %s", err.Error())
+				}
+				f.SetCellHyperLink(activeSheet, col, data, "External")
+				continue
+			}
+			f.SetCellValue(activeSheet, col, data)
+		}
+	}
+	f.SetActiveSheet(index)
+	// 保存文件
+	if err := f.SaveAs("output.xlsx"); err != nil {
+		log.Fatalf("Save file failed, %s", err)
+	}
+	fmt.Println("Excel file created successfully.")
+}
+
+// 读取远程图片
+func ReadRemoteFile(url string) ([]byte, error) {
+	result := make([]byte, 0)
+	if url == "" {
+		return result, errors.New("URL不能为空")
+	}
+
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	resp, err := netClient.Get(url)
+	if err != nil {
+		return result, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+func GetImageCol(headers []string) string {
+	index := -1
+	for i, header := range headers {
+		if header == "图片" {
+			index = i
+		}
+	}
+	if index > -1 {
+		col, _ := excelizev2.ColumnNumberToName(index + 1)
+		return col
+	}
+	return ""
+}
+
+func GetImageExt(path string) string {
+	// 查找最后一个 "." 的位置
+	lastDotIndex := strings.LastIndex(path, ".")
+
+	// 如果没有找到 "." 或者 "." 在字符串开头，则返回空字符串
+	if lastDotIndex == -1 || lastDotIndex == 0 {
+		return ".jpg"
+	}
+
+	// 提取 "." 之后的部分
+	ext := path[lastDotIndex:]
+	return strings.ToLower(ext)
+}
--- go.mod
+++ go.mod
@@ -19,9 +19,11 @@ require (
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/robfig/cron/v3 v3.0.1
+	github.com/sirupsen/logrus v1.9.3
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.15.0
+	github.com/xuri/excelize/v2 v2.8.1
 	go.uber.org/zap v1.24.0
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -32,19 +34,16 @@ require (
 	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/xuri/efp v0.0.0-20231025114914-d1ff6096ae53 // indirect
-	github.com/xuri/excelize/v2 v2.8.1 // indirect
 	github.com/xuri/nfp v0.0.0-20230919160717-d98342af3f05 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/sync v0.7.0 // indirect
 )
 
 require (
-	github.com/aws/aws-lambda-go v1.47.0
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
